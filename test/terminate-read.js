24bf3a2afd9454e3f37b15cc173171395a02695f
Dominic Tarr <dominic.tarr@gmail.com>
2016-02-02 14:07:36 +1300

    check that aborting always works in the right way

--- test/terminate-read.js
+++ test/terminate-read.js
@@ -2,6 +2,9 @@ var path = require('path');
 var test = require('tape');
 var pull = require('pull-stream');
 var file = require('..');
+var fs = require('fs')
+
+var ipsum = path.resolve(__dirname, 'assets', 'ipsum.txt')
 
 test('can terminate read process', function(t) {
   var expected = [
@@ -11,10 +14,8 @@ test('can terminate read process', function(t) {
     'les. Suspendisse cursus, turpis eget dap'
   ];
 
-//  t.plan(expected.length);
-
   pull(
-    file(path.resolve(__dirname, 'assets', 'ipsum.txt'), { bufferSize: 40 }),
+    file(ipsum, { bufferSize: 40 }),
     pull.take(expected.length),
     pull.drain(function(data) {
       t.equal(data.toString(), expected.shift(), 'line ok in drain');
@@ -24,3 +25,107 @@ test('can terminate read process', function(t) {
     })
   );
 });
+
+
+test('can terminate file immediately (before open)', function (t) {
+
+  var source = file(ipsum)
+  var sync = false
+  source(true, function (end) {
+    sync = true
+    t.equal(end, true)
+  })
+  t.ok(sync)
+  t.end()
+
+})
+
+
+test('can terminate file immediately (after open)', function (t) {
+
+  var source = file(ipsum)
+  var sync1 = false, sync2 = false
+  t.plan(6)
+  source(null, function (end, data) {
+    sync1 = true
+    t.equal(end, true, 'read aborted, end=true')
+    t.notOk(data, 'read aborted, data = null')
+  })
+  source(true, function (end) {
+    sync2 = true
+    t.ok(sync1, 'read cb was first')
+    t.equal(end, true)
+    t.end()
+  })
+  t.notOk(sync1)
+  t.notOk(sync2)
+
+})
+
+test('can terminate file during a read', function (t) {
+
+  var source = file(ipsum)
+  var sync1 = false, sync2 = false
+  source(null, function (end, data) {
+    t.equal(end, null)
+    t.ok(data)
+    source(null, function (end, data) {
+      sync1 = true
+      t.equal(end, true)
+      //t.equal(end, true)
+      t.notOk(data, "data can't have been read")
+    })
+    source(true, function (end) {
+      sync2 = true
+      t.equal(end, true)
+      t.ok(sync1, 'read called back first')
+      t.end()
+    })
+    t.notOk(sync1)
+    t.notOk(sync2)
+  })
+
+})
+
+
+//usually the read succeeds before the close does,
+//but not always
+
+test('after 10k times, cb order is always correct', function (t) {
+
+  var C = 0, R = 0, T = 0
+  ;(function next () {
+    T++
+
+    if(T > 10000) {
+      t.equal(R, 10000)
+      t.equal(C, 0)
+      t.equal(R+C, 10000)
+      console.log(C, R, T)
+      return t.end()
+    }
+
+    var fd = fs.openSync(__filename, 'r+', 0666)
+    var data, closed
+
+    //create a file stream with a fixed fd,
+    //configured to automatically close (as by default)
+    var source = file(null, {fd: fd})
+
+    //read.
+    source(null, function (err, _data) {
+      data = true
+      if(!closed) R++
+      if(data && closed) next()
+    })
+
+    //abort.
+    source(true, function (err) {
+      closed = true
+      if(!data) C ++
+      if(data && closed) next()
+    })
+  })()
+
+})
+
