f6369dcd12d6d39517e1d3cb6ea224038db8d472
Dominic Tarr <dominic.tarr@gmail.com>
2016-09-07 21:06:46 +1200

    more readable test

--- test/append.js
+++ test/append.js
@@ -12,24 +12,37 @@ tape('append to a file', function (t) {
   var n = 10, r = 0, ended = false
   ;(function next () {
     --n
-    fs.appendFile(filename, Date.now() +'\n', function (e) {
-      if(n) setTimeout(next, 20).unref()
-      else { ended = true; drain.abort() }
+    fs.appendFile(filename, Date.now() +'\n', function (err) {
+      if(err) throw err
+
+      if(n) setTimeout(next, 20)
+      else { ended = true; }
     })
   })()
 
-  var drain = pull.drain(function (chunk) {
-    r ++
-    t.notEqual(chunk.length, 0)
-  }, function (err) {
-    t.equal(n, 0, 'writes')
-    t.equal(r, 10, 'reads')
-    t.end()
-  })
-
-
-  pull(File(filename, {live: true}), drain)
+  pull(
+    File(filename, {live: true}),
+    pull.through(function (chunk) {
+      r ++
+      t.notEqual(chunk.length, 0)
+    }),
+    pull.take(10),
+    pull.drain(null, function (err) {
+      if(err) throw err
+      t.equal(n, 0, 'writes')
+      t.equal(r, 10, 'reads')
+      t.end()
+    })
+  )
 })
+
+
+
+
+
+
+
+
 
 
 
