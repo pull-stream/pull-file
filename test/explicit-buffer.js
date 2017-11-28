70a34c8ff61c5490c2f8e4925acc17523e508bc3
Emil Bay <github@tixz.dk>
2017-11-28 21:46:13 +0900

    Remove test image, create tmp file

--- test/explicit-buffer.js
+++ test/explicit-buffer.js
@@ -9,22 +9,19 @@ var fs = require('fs')
 
 var tmpfile = path.join(osenv.tmpdir(), 'test_pull-file_big')
 
+var big = crypto.pseudoRandomBytes(10*1024*1024)
+fs.writeFileSync(tmpfile, big)
+
 function hash (data) {
   return crypto.createHash('sha256').update(data).digest('hex')
-}
-
-function asset(file) {
-  return path.join(__dirname, 'assets', file)
 }
 
 test('large file in explicit buffer', function(t) {
   var buf = new Buffer(65551) // prime close to 1024 * 64
   var h = crypto.createHash('sha256')
 
-  var big = fs.readFileSync(asset('img.jpg'))
-
   pull(
-    file(asset('img.jpg'), {buffer: buf}),
+    file(tmpfile, {buffer: buf}),
     pull.through(function (chunk) {
       h.update(chunk)
     }),
